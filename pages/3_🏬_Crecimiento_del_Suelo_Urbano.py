75b36514c77a8734d7a6e2e547a7d39f6907aba2
cqsalazar <cristianq015@gmail.com>
2025-04-24 14:24:56 -0500

    Update 3_🏬_Crecimiento_del_Suelo_Urbano.py

--- pages/3_🏬_Crecimiento_del_Suelo_Urbano.py
+++ pages/3_🏬_Crecimiento_del_Suelo_Urbano.py
@@ -106,7 +106,7 @@ m.add_gdf(area_exp, layer_name='Área de Expansión', style={'color':'olive', 'f
 m.add_gdf(perim_mun, layer_name='Perímetro Municipal', style={'color':'silver', 'fill': None, 'weight':2})
 
 style = {"stroke": True, "fillOpacity": 1, 'weight':0}
-callback = lambda feat: {"color": feat["properties"]["SIMBOLOGY"], "stroke": True, "fillOpacity": 1, 'weight':0}
+callback = lambda feat: {"color": feat["properties"]["SIMBOLOGY"], "stroke": True, "fillOpacity": 1, 'weight':0.1}
 
 #m.add_gdf(dif_a_cons, layer_name='Cambio en Área Construida', style=style, fill_colors=["#FFFFE5","#FEE391","#FE9929","#CC4C02","#662506"])
 
